9aac48c634e4cd5f60553d7ccdf187fa327c3ed3
theodore-dream <richardtheodorehyman@gmail.com>
2020-07-11 14:42:21 -0400

    added systemd unit file and have working config for a main.py and button17.py to take start service on boot and take photo when pressing button17

--- button17.py
+++ button17.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python
+
 from picamera import PiCamera
 from time import sleep
+import time
 
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.start_preview()
-sleep(2)
-camera.capture('/home/pi/Documents/images/image.jpg')
+timestr = time.strftime("%Y%m%d-%H%M%S")
+imagefolder = '/home/pi/Documents/images/'
+format = '.jpg'
 
+def image():
+   camera = PiCamera()
+   camera.resolution = (1920, 1080)
+   camera.start_preview()
+   sleep(2)
+   camera.capture(imagefolder + 'image' + timestr + format)
+   return;
+
+image()
 
 #cmd = "raspstill -vf -o /home/pi/Documents/images/image.jpeg"
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gpiozero import Button
 from time import sleep
+import subprocess
+
 
 button17 = Button(17)
 button22 = Button(22)
@@ -9,6 +11,7 @@ button27 = Button(27)
 while True:
     if button17.is_pressed:
         print("button17 pressed")
+        subprocess.call("/home/pi/Documents/raspi-hq-cam/button17.py", shell=True)
     if button22.is_pressed:
         print("button22 pressed")
     if button23.is_pressed:
