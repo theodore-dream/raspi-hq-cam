2769ced4f3e0bcb9d91a855a93694fc9e07d96a1
theodore-dream <richardtheodorehyman@gmail.com>
2020-07-27 17:26:06 -0400

    button22 modifications

--- button22.py
+++ button22.py
@@ -4,16 +4,19 @@ from picamera import PiCamera
 from time import sleep
 import time
 
+timestr = time.strftime("%Y%m%d-%H%M%S")
+imagefolder = '/home/pi/Documents/videos/'
+format = '.h264'
 
 
 
 def video():
    camera = PiCamera()
    camera.resolution = (1920, 1080)
-   camera.framerate = 25
+   camera.framerate = 22
    camera.start_preview()
    time.sleep(2)
-   camera.start_recording('my_video.h264', format='h264', quality=5)
+   camera.start_recording((imagefolder + 'video' + timestr + format), format='h264', quality=5)
    camera.wait_recording(30)
    camera.stop_recording()
    return;
